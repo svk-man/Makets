c84dc28b814c8ead1cadcf94bba2a52e910a118e
svk-man <kravchenko.sgv@gmail.com>
2020-02-25 22:31:28 +0400

    SberbankMainActivity: init

--- app/src/main/java/ru/junmidsen/makets/MainActivity.java
+++ app/src/main/java/ru/junmidsen/makets/MainActivity.java
@@ -2,13 +2,31 @@ package ru.junmidsen.makets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button showSberbankMainActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showSberbankMainActivityButton = (Button) findViewById(R.id.button_sberbank_main);
+        showSberbankMainActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSberbankMainActivity();
+            }
+        });
+    }
+
+    private void showSberbankMainActivity() {
+        Intent intent = new Intent(this, SberbankMainActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ru/junmidsen/makets/SberbankMainActivity.java
@@ -0,0 +1,14 @@
+package ru.junmidsen.makets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SberbankMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sberbank_main);
+    }
+}
